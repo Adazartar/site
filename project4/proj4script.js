846851d12f0605786e5dff1e8258fc05c65d0202
Adam Mantello <therealadazartar@gmail.com>
2023-02-19 10:48:50 +1100

    Can now add players

--- project4/proj4script.js
+++ project4/proj4script.js
@@ -1,1 +1,25 @@
 
+const initialConfig = "auto auto auto auto";
+document.querySelector('.grid-container').style.gridTemplateColumns = initialConfig;
+
+function insertAfter(el, referenceNode) {
+    referenceNode.parentNode.insertBefore(el, referenceNode.nextSibling);
+}
+
+const newPlayerBtn = document.querySelector('#newPlayer');
+newPlayerBtn.addEventListener('click',() => {
+    const rows = document.querySelectorAll('.grid-row');
+
+    for(let i = 0; i < rows.length; i++){
+
+        const div = document.createElement('div');
+        div.className = 'grid-item';
+        const input = document.createElement('input');
+        div.appendChild(input);
+
+        insertAfter(div, rows[i]);
+    }
+
+    const currentConfig = document.querySelector('.grid-container').style.gridTemplateColumns;
+    document.querySelector('.grid-container').style.gridTemplateColumns = currentConfig + " auto";
+});
